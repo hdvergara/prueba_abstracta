ac7ebf67fc8117215d0d636a4b8d521d114f30c4
Hector Vergara <ext.hdvergara@maxillc.com>
2025-03-03 16:15:38 -0500

    se hacen ajustes

--- src/main/java/framework/abstracta/manager/DriverManager.java
+++ src/main/java/framework/abstracta/manager/DriverManager.java
@@ -5,28 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
-
 public class DriverManager {
     private static WebDriver driver;
 
-    private DriverManager(){}
+    private DriverManager() {
+    }
 
     public static WebDriver getDriver() {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
-
-            // Descargar y configurar automáticamente el ChromeDriver
             WebDriverManager.chromedriver().setup();
-
-            // Configurar modo headless si es necesario
             if (System.getProperty("headless", "false").equals("true")) {
                 options.addArguments("--headless");
                 options.addArguments("--window-size=1920,1080");
                 options.addArguments("--disable-gpu");
                 options.addArguments("--no-sandbox");
             }
-
+            options.addArguments("--ignore-certificate-errors");
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
